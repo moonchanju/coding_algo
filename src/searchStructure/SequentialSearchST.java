0740e194ccaf79b86da6b1f2742e5d47a0eb5996
moonchanju <ckswn1244@gmail.com>
2026-03-28 22:14:45 +0900

    기본 노드 구조 생성

--- src/searchStructure/SequentialSearchST.java
+++ src/searchStructure/SequentialSearchST.java
@@ -1,4 +1,17 @@
 package searchStructure;
 
+class Node<K,V>
+{
+
+    K key; // 키
+    V val; // 값
+    Node <K,V> next; // 다음 노드를 가리키는 포인터
+    public Node(K key, V val, Node<K,V> next)
+    {
+        this.key = key;
+        this.val = val;
+        this.next = next;
+    }
+}
 public class SequentialSearchST {
 }
