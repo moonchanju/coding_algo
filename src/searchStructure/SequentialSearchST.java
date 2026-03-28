08191edc47fdb0ef903670ba31091a78a3c4d860
moonchanju <ckswn1244@gmail.com>
2026-03-28 22:26:17 +0900

    get 메소드 구현

--- src/searchStructure/SequentialSearchST.java
+++ src/searchStructure/SequentialSearchST.java
@@ -1,17 +1,36 @@
 package searchStructure;
 
-class Node<K,V>
-{
+class Node<K, V> {
 
     K key; // 키
     V val; // 값
-    Node <K,V> next; // 다음 노드를 가리키는 포인터
-    public Node(K key, V val, Node<K,V> next)
-    {
+    Node<K, V> next; // 다음 노드를 가리키는 포인터
+
+    public Node(K key, V val, Node<K, V> next) {
         this.key = key;
         this.val = val;
         this.next = next;
     }
 }
-public class SequentialSearchST {
+
+//클래스에서 제네릭 사용 시 제네릭 선언이 필수 -> <k,v>
+public class SequentialSearchST <K,V> {
+    private Node<K,V> first;
+    int N;
+
+    // get 메소드 구현
+    // 찾고하 하는 key 존재시 해당 key 의 val 반환, 없을 시 null 반환
+    public V get(K key) {
+        for(Node <K,V> x=first;x!=null; x = x.next)
+        {
+            if(key.equals(x.key))
+            {
+                return x.val;
+            }
+        }
+        return null;
+    }
+
 }
+
+
