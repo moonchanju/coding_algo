e6848344eaabe7ae4a1df1e2b362eb71c2845c32
moonchanju <ckswn1244@gmail.com>
2026-03-29 17:25:41 +0900

    search 메소드 구현

--- src/searchStructure/BinarySearchST.java
+++ src/searchStructure/BinarySearchST.java
@@ -39,13 +39,12 @@ public class BinarySearchST<K extends Comparable<K>, V>
 
     //배열 크기를 동적으로 변경하는 resize
     //capacity 만큼의 크기를 만들고, 기존 배열을 덮어 씌우는 기능
-    private void resize(int capacity)
-    {
+    private void resize(int capacity) {
         //배열 크기 동적 할당
         K[] tempk = (K[]) new Comparable[capacity];
         V[] tempv = (V[]) new Object[capacity];
 
-        for(int i=0;i<N;i++)
+        for (int i = 0; i < N; i++)
         //실제로 저장된 key-val 쌍의 개수를 의미하는 N
         {
             tempk[i] = keys[i];
@@ -55,5 +54,28 @@ public class BinarySearchST<K extends Comparable<K>, V>
         //기존 keys,vals 포인터 수정
         keys = tempk;
         vals = tempv;
+    }
+
+    private int search(K key) {
+        int low = 0;
+        int high = N - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            //찾으려는 key가 keys 배열의 중앙과 비교한 상대 위치를 정수로 받음
+
+            if (cmp < 0) {
+                high = mid - 1;
+            }
+            else if (cmp > 0) {
+                low = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return low;
+        //찾으려는 값이 없다면 새로 삽입해야할 단계임으로 low 위치에 삽입할 수 있도록 low 반환
     }
 }
