9c0f2c9bf5bcb561d479245de63fe9695db05cf5
moonchanju <ckswn1244@gmail.com>
2026-03-30 20:11:47 +0900

    노드 기본 구조 제작

--- src/searchStructure/BST.java
+++ src/searchStructure/BST.java
@@ -1,4 +1,17 @@
 package searchStructure;
 
+class Node<K,V> {
+    int N; // 자손 노드 + 1 (ordered 연산)
+    int aux: // AVL 트리나 RB 트리에 사용
+    Node<K,V> parent; // AVL or RB
+    public Node(K key, V val) {
+        this.key = key; this.value = val;
+        this.N = 1;
+    }
+    public int getAux(){ return aux; }
+    public void setAux(int value) { aux = value; }
+}
+}
+
 public class BST {
 }
