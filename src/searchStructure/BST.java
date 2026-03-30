62f5feb4afe04e0d690f2254e774c150bacf8400
moonchanju <ckswn1244@gmail.com>
2026-03-30 20:42:56 +0900

    get 메서드 구현

--- src/searchStructure/BST.java
+++ src/searchStructure/BST.java
@@ -4,6 +4,7 @@ package searchStructure;
 public class BST<K extends Comparable<K>, V> {
     protected Node<K, V> root;
 
+    //treeSearch
     //키를 입력 받아 -> 그 키를 가지는 노드 또는 노드를 가지지 않으면 순회의 마지막 노드를 반환하자 (삽입을 위해서)
     protected Node<K, V> treeSearch(K key) {
         Node<K, V> x = root;
@@ -29,5 +30,20 @@ public class BST<K extends Comparable<K>, V> {
                 }
             }
         }
+    }
+
+    //get
+    //주어진 Key 에 해당하는 값을 반환 , 키가 없으면 Null
+    public V get(K key)
+    {
+        if(root == null) return null;
+        Node<K,V> x = treeSearch(key);
+        if(key.equals(x.key)) return x.val;
+        //treeSearch 반환값 기준 equals 로 비교
+        else
+        {
+            return null;
+        }
+
     }
 }
