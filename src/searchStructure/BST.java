0044d44ab543f891271cef52996c231422b5ae87
moonchanju <ckswn1244@gmail.com>
2026-03-31 15:02:53 +0900

    delete 메소드 삭제 노드가 자식인 경우 처리및 메소드 구현 완료

--- src/searchStructure/BST.java
+++ src/searchStructure/BST.java
@@ -124,22 +124,19 @@ public class BST<K extends Comparable<K>, V> {
         if (!key.equals(x.key)) return; // 해당 노드가 존재하지 않을 경우 return
 
         //but ! 해당 코드는 루트인 부분과 자식 2개인 부분을 한대 묶어 if 로 처리
-        if (x == root || isTwoNode(x))
-        //root 이거나 자식이 두개인 경우
+        if (x == root || isTwoNode(x)) //root 이거나 자식이 두개인 경우
         {
             // 루트이며 리프 -> 루트 삭제후 종료
             if (isLeaf(x)) {
                 root = null;
                 return;
             }
-
             //루트이며 자식이 하나 -> 자식을 루트로 올리고 해당 자식의 부모 포인터를 null
             else if (!isTwoNode(x)) {
                 root = (x.left == null) ? x.right : x.left; //삼항 연산자로 자식의 위치를 찾아 root 로 올리기
                 root.parent = null;
                 return;
             }
-
             // 자식이 둘 ( 루트이며 자식이 둘인 경우도 포함이 됨)
             // successor 을 찾아서 root의 자리에 올리는 역할
             else {
@@ -153,14 +150,27 @@ public class BST<K extends Comparable<K>, V> {
                 x.val = y.val;
 
                 //successor의 부모인 p -> successor의 자식과 이어주는 역할을 함
-                p= y.parent;
+                p = y.parent;
                 //relink 는 부모와 자식을 연결해주는 작업을 함, parent , child, makeleft(어느쪽 자식인가?) 의 인자를 가져감.
                 //y.right를 자식으로 넣은 이유 -> successor은 우측 자식만 있을수있음.
-                relink(p,y.right,y==p.left); //
+                relink(p, y.right, y == p.left); //
                 rebalance(p.y);
             }
-
-
+        }
+        else // 자식이 1개 또는 0개인 경우 처리
+        //루트가 아님 , 즉 삭제할 시 본인의 자식이 있다면 부모와 연결 시켜주는 작업이 필요.]
+        {
+            //relink 사용 방식 -> 삭제할 노드의 부모 변수 설정, 삭제할 자식 변수 설정, 왼쪽 자식인가에 대한 매개변수 설정
+            //자식이 없는 경우는 x.left , x.right 가 null로 처리 되므로 -> 자연스레 p와 null이 이어지므로 삭제 처리
+            p = x.parent;
+            if (x.right == null) {
+                relink(p,x.left,p==x.left);
+            }
+            else if(x.left==null)
+            {
+                relink(p,x.right,p==x.right);
+            }
+            rebalanceDelete(p,x);
         }
     }
 
